a8cc46ea0e933f4807146d39a309d5d9b30acb4b
Rob Hess <hessro@oregonstate.edu>
2023-11-08 14:51:30 -0800

    Add code from 11/8 lecture.

--- wordBoxes.js
+++ wordBoxes.js
@@ -9,7 +9,7 @@ function handleNewWordsEntered(event) {
   allWords = text.replace(/[!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~]/g, '')
     .toLowerCase().split(' ');
   console.log("  -- allWords:", allWords)
-    // currentWord = 0;
+  currentWord = 0;
 }
 
 function generateWordElem(word, highlightColor) {
@@ -23,9 +23,42 @@ function generateWordElem(word, highlightColor) {
   return wordElem;
 }
 
+function getHighlightColor() {
+  var selectedRadioButton = document.querySelector(
+    "input[name='highlight-color']:checked"
+  )
+  console.log("== selectedRadioButton:", selectedRadioButton)
+  return selectedRadioButton.value
+}
+
+function addWordButtonClickHandler(event) {
+  var word = allWords[currentWord]
+  if (word) {
+    var clickedButton = event.target
+    var container = clickedButton.parentNode.parentNode
+    var wordsContainer = container.getElementsByClassName("words-container")[0]
+
+    var everyNthSelect = document.getElementById("every-nth-select")
+    var everyNth = parseInt(everyNthSelect.value)
+    var highlightColor = null
+    if (!((currentWord + 1) % everyNth)) {
+      highlightColor = getHighlightColor()
+    }
+
+    var wordElem = generateWordElem(word, highlightColor)
+    wordsContainer.appendChild(wordElem)
+    currentWord = (currentWord + 1) % allWords.length
+  }
+}
+
 var wordsEntryArea = document.getElementById("words-input")
 wordsEntryArea.addEventListener(
   // "input",
   "change",
   handleNewWordsEntered
 )
+
+var addWordButtons = document.getElementsByClassName("add-word-button")
+for (var i = 0; i < addWordButtons.length; i++) {
+  addWordButtons[i].addEventListener("click", addWordButtonClickHandler)
+}
